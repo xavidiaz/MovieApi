1e0da518b4e1c0d35f374a2276b7e761ccc4aeb3
Javier Diaz Urbano <me@xavidiaz.com>
2026-08-19 13:12:23 +0200

    Del 2: add Movie entity in Entities/ folder with data annotations. Closes #2

---
+++ Entities/Movie.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.Entities;
+
+public class Movies
+{
+    public int Id { get; set; }
+
+    [Required, StringLength(200)]
+    public required string Title { get; set; }
+
+    [Required, StringLength(200)]
+    public required string Genre { get; set; }
+
+    [Required, StringLength(100)]
+    public required string Director { get; set; }
+
+    [Range(1888, 2100)]
+    public int ReleaseYear { get; set; }
+
+    [Range(1, int.MaxValue)]
+    public int DurationMinutes { get; set; }
+
+    [Range(0, 10)]
+    public double Rating { get; set; }
+
+    public string? Description { get; set; }
+
+}
