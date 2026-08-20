6b2d90f0e89f54a4afe608bc61b27d5c8bf6ff49
Javier Diaz Urbano <me@xavidiaz.com>
2026-08-20 10:06:19 +0200

    Del 9: enable Swagger UI on top of built-in OpenAPI. Closes #8

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(o => o.SwaggerEndpoint("/openapi/v1.json", "v1"));
 }
 
 app.UseHttpsRedirection();
